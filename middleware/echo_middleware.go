8855b87685be059b2b4c8282e9b3ec1e944bdaeb
Bagas Prabaswara <bagasprabaswara77@gmail.com>
2023-04-03 11:15:36 +0700

    feature: profile

--- middleware/echo_middleware.go
+++ middleware/echo_middleware.go
@@ -11,39 +11,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// func VerifyToken(next echo.HandlerFunc) echo.HandlerFunc {
-// 	return func(c echo.Context) error {
-// 		authHeader := c.Request().Header.Get("Authorization")
-// 		if authHeader == "" {
-// 			return c.JSON(http.StatusUnauthorized, echo.Map{"error": "missing Authorization header"})
-// 		}
-
-// 		tokenString := strings.Split(authHeader, " ")[1]
-// 		claims := &model.Claims{}
-
-// 		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-// 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-// 				return nil, echo.NewHTTPError(http.StatusBadRequest, "invalid token")
-// 			}
-// 			return config.JWTKey(), nil
-// 		})
-
-// 		if err != nil {
-// 			if err == jwt.ErrSignatureInvalid {
-// 				return c.JSON(http.StatusUnauthorized, echo.Map{"error": "invalid token signature"})
-// 			}
-// 			return c.JSON(http.StatusUnauthorized, echo.Map{"error": "invalid token"})
-// 		}
-
-// 		if !token.Valid {
-// 			return c.JSON(http.StatusUnauthorized, echo.Map{"error": "invalid token"})
-// 		}
-
-// 		c.Set("userID", claims.UserID)
-// 		return next(c)
-// 	}
-// }
-
 func MustAuthenticateAccessToken() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
--- router/user_router.go
+++ router/user_router.go
@@ -10,4 +10,6 @@ func (r *router) userRouter() {
 	r.group.GET("/user/:id", r.userController.HandleFindAdminByID(), middleware.MustAuthenticateAccessToken(), middleware.MustSuperAdminOnly())
 	r.group.PUT("/user/:id", r.userController.HandleUpdateAdmin(), middleware.MustAuthenticateAccessToken(), middleware.MustSuperAdminOnly())
 	r.group.DELETE("/user/:id", r.userController.HandleDeleteAdminByID(), middleware.MustAuthenticateAccessToken(), middleware.MustSuperAdminOnly())
+	r.group.GET("/user/profile", r.userController.HandleProfile(), middleware.MustAuthenticateAccessToken())
+	r.group.PUT("/user/profile", r.userController.HandleUpdateProfile(), middleware.MustAuthenticateAccessToken())
 }
