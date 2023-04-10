47cc745dfb91399cdfb2a724214011c50d83d0a4
Bagas Prabaswara <bagasprabaswara77@gmail.com>
2023-04-10 21:42:25 +0700

    feature: configuration

--- model/configuration.go
+++ model/configuration.go
@@ -1,15 +1,51 @@
 package model
 
-import "time"
+import (
+	"context"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type CreateUpdateConfigurationRequest struct {
+	DietClassifierEpoch        int32   `json:"diet_classifier_epoch" validate:"required"`
+	FallbackClassifierTreshold float32 `json:"fallback_classifier_treshold" validate:"required"`
+	ResponseSelectorEpoch      int32   `json:"response_selector_epoch" validate:"required"`
+	TedPolicyEpoch             int32   `json:"ted_policy_epoch" validate:"required"`
+	FallbackUtteranceId        int64   `json:"fallback_utterance_id" validate:"required"`
+	FallbackTreshold           float32 `json:"fallback_treshold" validate:"required"`
+}
+
+func (c *CreateUpdateConfigurationRequest) Validate() error {
+	return validate.Struct(c)
+}
 
 type Configuration struct {
 	Id                         int64     `json:"id"`
-	DietClassifierEpoch        int32     `json:"dietclassifier_epoch"`
-	FallbackClassifierTreshold int32     `json:"fallbackclassifier_treshold"`
-	ResponseSelectorEpoch      int32     `json:"responseselector_epoch"`
-	TedPolicyEpoch             int32     `json:"tedpolicy_epoch"`
+	DietClassifierEpoch        int32     `json:"diet_classifier_epoch"`
+	FallbackClassifierTreshold float32   `json:"fallback_classifier_treshold"`
+	ResponseSelectorEpoch      int32     `json:"response_selector_epoch"`
+	TedPolicyEpoch             int32     `json:"ted_policy_epoch"`
 	FallbackUtteranceId        int64     `json:"fallback_utterance_id"`
-	FallbackTreshold           int32     `json:"fallback_treshold"`
+	FallbackTreshold           float32   `json:"fallback_treshold"`
 	CreatedAt                  time.Time `json:"created_at"`
 	UpdatedAt                  time.Time `json:"updated_at"`
+}
+
+type ConfigurationController interface {
+	HandleCreateConfiguration() echo.HandlerFunc
+	HandleFindConfiguration() echo.HandlerFunc
+	HandleUpdateConfiguration() echo.HandlerFunc
+}
+
+type ConfigurationService interface {
+	CreateConfiguration(ctx context.Context, req CreateUpdateConfigurationRequest) (*Configuration, error)
+	FindConfiguration(ctx context.Context, id int64) (*Configuration, error)
+	UpdateConfiguration(ctx context.Context, id int64, req CreateUpdateConfigurationRequest) (*Configuration, error)
+}
+
+type ConfigurationRepository interface {
+	Create(ctx context.Context, configuration *Configuration) error
+	FindByID(ctx context.Context, id int64) (*Configuration, error)
+	Update(ctx context.Context, id int64, configuration *Configuration) error
 }
---
+++ service/configuration_service.go
@@ -0,0 +1,132 @@
+package service
+
+import (
+	"cbupnvj/constant"
+	"cbupnvj/helper"
+	"cbupnvj/model"
+	"context"
+
+	"github.com/sirupsen/logrus"
+)
+
+type configurationService struct {
+	configurationRepository model.ConfigurationRepository
+	utteranceRepository     model.UtteranceRepository
+}
+
+func NewConfigurationService(configurationRepository model.ConfigurationRepository, utteranceRepository model.UtteranceRepository) model.ConfigurationService {
+	return &configurationService{
+		configurationRepository: configurationRepository,
+		utteranceRepository:     utteranceRepository,
+	}
+}
+
+func (c *configurationService) CreateConfiguration(ctx context.Context, req model.CreateUpdateConfigurationRequest) (*model.Configuration, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"ctx":     ctx,
+		"request": req,
+	})
+
+	if err := req.Validate(); err != nil {
+		log.Error(err)
+		return nil, constant.HttpValidationOrInternalErr(err)
+	}
+
+	utterance, err := c.utteranceRepository.FindByID(ctx, req.FallbackUtteranceId)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	if utterance == nil {
+		log.Error(constant.ErrNotFound)
+		return nil, constant.ErrNotFound
+	}
+
+	conf := &model.Configuration{
+		Id:                         helper.GenerateID(),
+		DietClassifierEpoch:        req.DietClassifierEpoch,
+		FallbackClassifierTreshold: req.FallbackClassifierTreshold,
+		ResponseSelectorEpoch:      req.ResponseSelectorEpoch,
+		TedPolicyEpoch:             req.TedPolicyEpoch,
+		FallbackUtteranceId:        req.FallbackUtteranceId,
+		FallbackTreshold:           req.FallbackTreshold,
+	}
+
+	err = c.configurationRepository.Create(ctx, conf)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return conf, nil
+}
+
+func (c *configurationService) FindConfiguration(ctx context.Context, id int64) (*model.Configuration, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"ctx": ctx,
+		"id":  id,
+	})
+
+	if id <= 0 {
+		return nil, constant.ErrInvalidArgument
+	}
+
+	conf, err := c.configurationRepository.FindByID(ctx, id)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	if conf == nil {
+		log.Error(constant.ErrNotFound)
+		return nil, constant.ErrNotFound
+	}
+
+	return conf, nil
+}
+
+func (c *configurationService) UpdateConfiguration(ctx context.Context, id int64, req model.CreateUpdateConfigurationRequest) (*model.Configuration, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"ctx": ctx,
+		"id":  id,
+		"req": req,
+	})
+
+	if err := req.Validate(); err != nil {
+		log.Error(err)
+		return nil, constant.HttpValidationOrInternalErr(err)
+	}
+
+	conf, err := c.FindConfiguration(ctx, id)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	utterance, err := c.utteranceRepository.FindByID(ctx, req.FallbackUtteranceId)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	if utterance == nil {
+		log.Error(constant.ErrNotFound)
+		return nil, constant.ErrNotFound
+	}
+
+	conf.FallbackUtteranceId = req.FallbackUtteranceId
+	conf.DietClassifierEpoch = req.DietClassifierEpoch
+	conf.FallbackClassifierTreshold = req.FallbackClassifierTreshold
+	conf.FallbackTreshold = req.FallbackTreshold
+	conf.ResponseSelectorEpoch = req.ResponseSelectorEpoch
+	conf.TedPolicyEpoch = req.TedPolicyEpoch
+
+	err = c.configurationRepository.Update(ctx, conf.Id, conf)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return conf, nil
+}
