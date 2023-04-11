7ead8fc6cbd64841989aced6ba9f1dded2e8c131
Bagas Prabaswara <bagasprabaswara77@gmail.com>
2023-04-11 20:16:28 +0700

    feature: training history

--- model/training_history.go
+++ model/training_history.go
@@ -1,6 +1,11 @@
 package model
 
-import "time"
+import (
+	"context"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
 
 type Status string
 
@@ -9,10 +14,35 @@ const (
 	StatusFailed Status = "FAILED"
 )
 
+type CreateTrainingHistoryRequest struct {
+	UserId    int64  `json:"user_id" validate:"required"`
+	TotalTime int    `json:"total_time" validate:"required"`
+	Status    Status `json:"status" validate:"required"`
+}
+
+func (c *CreateTrainingHistoryRequest) Validate() error {
+	return validate.Struct(c)
+}
+
 type TrainingHistory struct {
-	Id        int64     `json:"int64"`
+	Id        int64     `json:"id"`
 	UserId    int64     `json:"user_id"`
 	TotalTime int       `json:"total_time"`
 	Status    Status    `json:"status"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+type TrainingHistoryController interface {
+	HandleCreateTrainingHistory() echo.HandlerFunc
+	HandleFindAllTrainingHistory() echo.HandlerFunc
+}
+
+type TrainingHistoryService interface {
+	CreateTrainingHistory(ctx context.Context, req CreateTrainingHistoryRequest) (*TrainingHistory, error)
+	FindAllTrainingHistory(ctx context.Context) ([]*TrainingHistory, error)
+}
+
+type TrainingHistoryRepository interface {
+	Create(ctx context.Context, th *TrainingHistory) error
+	FindAll(ctx context.Context) ([]*TrainingHistory, error)
 }
---
+++ repository/training_history_repository.go
@@ -0,0 +1,49 @@
+package repository
+
+import (
+	"cbupnvj/model"
+	"context"
+
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+type trainingHistoryRepository struct {
+	db *gorm.DB
+}
+
+func NewTrainingHistoryRepository(db *gorm.DB) model.TrainingHistoryRepository {
+	return &trainingHistoryRepository{
+		db: db,
+	}
+}
+
+func (th *trainingHistoryRepository) Create(ctx context.Context, trainingHistory *model.TrainingHistory) error {
+	log := logrus.WithFields(logrus.Fields{
+		"ctx":             ctx,
+		"trainingHistory": trainingHistory,
+	})
+
+	err := th.db.WithContext(ctx).Create(trainingHistory).Error
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	return nil
+}
+
+func (th *trainingHistoryRepository) FindAll(ctx context.Context) ([]*model.TrainingHistory, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"ctx": ctx,
+	})
+
+	var trainingHistories []*model.TrainingHistory
+	res := th.db.WithContext(ctx).Find(&trainingHistories)
+	if res.Error != nil {
+		log.Error(res.Error)
+		return nil, res.Error
+	}
+
+	return trainingHistories, nil
+}
