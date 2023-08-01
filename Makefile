af7f448026f215c91753b6be3a47e9f739239ef9
Bagas Prabaswara <bagasprabaswara77@gmail.com>
2023-08-01 14:54:48 +0700

    feature: mock unit test for repo and service

--- Makefile
+++ Makefile
@@ -6,18 +6,73 @@ model/mock/mock_session_repository.go:
 	mockgen -destination=model/mock/mock_session_repository.go -package=mock cbupnvj/model SessionRepository
 model/mock/mock_intent_repository.go:
 	mockgen -destination=model/mock/mock_intent_repository.go -package=mock cbupnvj/model IntentRepository
+model/mock/mock_action_http_repository.go:
+	mockgen -destination=model/mock/mock_action_http_repository.go -package=mock cbupnvj/model ActionHttpRepository
+model/mock/mock_configuration_repository.go:
+	mockgen -destination=model/mock/mock_configuration_repository.go -package=mock cbupnvj/model ConfigurationRepository	
+model/mock/mock_entity_repository.go:
+	mockgen -destination=model/mock/mock_entity_repository.go -package=mock cbupnvj/model EntityRepository
+model/mock/mock_example_repository.go:
+	mockgen -destination=model/mock/mock_example_repository.go -package=mock cbupnvj/model ExampleRepository
+model/mock/mock_faculty_repository.go:
+	mockgen -destination=model/mock/mock_faculty_repository.go -package=mock cbupnvj/model FacultyRepository
+model/mock/mock_fallback_chat_log_repository.go:
+	mockgen -destination=model/mock/mock_fallback_chat_log_repository.go -package=mock cbupnvj/model FallbackChatLogRepository
+model/mock/mock_log_intent_repository.go:
+	mockgen -destination=model/mock/mock_log_intent_repository.go -package=mock cbupnvj/model LogIntentRepository
+model/mock/mock_major_repository.go:
+	mockgen -destination=model/mock/mock_major_repository.go -package=mock cbupnvj/model MajorRepository
+model/mock/mock_req_body_repository.go:
+	mockgen -destination=model/mock/mock_req_body_repository.go -package=mock cbupnvj/model ReqBodyRepository
+model/mock/mock_rule_repository.go:
+	mockgen -destination=model/mock/mock_rule_repository.go -package=mock cbupnvj/model RuleRepository
+model/mock/mock_step_repository.go:
+	mockgen -destination=model/mock/mock_step_repository.go -package=mock cbupnvj/model StepRepository
+model/mock/mock_story_repository.go:
+	mockgen -destination=model/mock/mock_story_repository.go -package=mock cbupnvj/model StoryRepository
+model/mock/mock_training_history_repository.go:
+	mockgen -destination=model/mock/mock_training_history_repository.go -package=mock cbupnvj/model TrainingHistoryRepository
+model/mock/mock_utterance_repository.go:
+	mockgen -destination=model/mock/mock_utterance_repository.go -package=mock cbupnvj/model UtteranceRepository
+
 model/mock/mock_user_service.go:
 	mockgen -destination=model/mock/mock_user_service.go -package=mock cbupnvj/model UserService
 model/mock/mock_auth_service.go:
 	mockgen -destination=model/mock/mock_auth_service.go -package=mock cbupnvj/model AuthService
 model/mock/mock_intent_service.go:
 	mockgen -destination=model/mock/mock_intent_service.go -package=mock cbupnvj/model IntentService
-model/mock/mock_user_controller.go:
-	mockgen -destination=model/mock/mock_user_controller.go -package=mock cbupnvj/model UserController
-model/mock/mock_auth_controller.go:
-	mockgen -destination=model/mock/mock_auth_controller.go -package=mock cbupnvj/model AuthController
-model/mock/mock_intent_controller.go:
-	mockgen -destination=model/mock/mock_intent_controller.go -package=mock cbupnvj/model IntentController
+model/mock/mock_action_http_service.go:
+	mockgen -destination=model/mock/mock_action_http_service.go -package=mock cbupnvj/model ActionHttpService
+model/mock/mock_configuration_service.go:
+	mockgen -destination=model/mock/mock_configuration_service.go -package=mock cbupnvj/model ConfigurationService
+model/mock/mock_conversation_service.go:
+	mockgen -destination=model/mock/mock_conversation_service.go -package=mock cbupnvj/model ConversationService
+model/mock/mock_entity_service.go:
+	mockgen -destination=model/mock/mock_entity_service.go -package=mock cbupnvj/model EntityService
+model/mock/mock_example_service.go:
+	mockgen -destination=model/mock/mock_example_service.go -package=mock cbupnvj/model ExampleService
+model/mock/mock_faculty_service.go:
+	mockgen -destination=model/mock/mock_faculty_service.go -package=mock cbupnvj/model FacultyService
+model/mock/mock_fallback_chat_log_service.go:
+	mockgen -destination=model/mock/mock_fallback_chat_log_service.go -package=mock cbupnvj/model FallbackChatLogService
+model/mock/mock_log_intent_service.go:
+	mockgen -destination=model/mock/mock_log_intent_service.go -package=mock cbupnvj/model LogIntentService
+model/mock/mock_major_service.go:
+	mockgen -destination=model/mock/mock_major_service.go -package=mock cbupnvj/model MajorService
+model/mock/mock_req_body_service.go:
+	mockgen -destination=model/mock/mock_req_body_service.go -package=mock cbupnvj/model ReqBodyService
+model/mock/mock_rule_service.go:
+	mockgen -destination=model/mock/mock_rule_service.go -package=mock cbupnvj/model RuleService
+model/mock/mock_step_service.go:
+	mockgen -destination=model/mock/mock_step_service.go -package=mock cbupnvj/model StepService
+model/mock/mock_story_service.go:
+	mockgen -destination=model/mock/mock_story_service.go -package=mock cbupnvj/model StoryService
+model/mock/mock_training_history_service.go:
+	mockgen -destination=model/mock/mock_training_history_service.go -package=mock cbupnvj/model TrainingHistoryService
+model/mock/mock_utterance_service.go:
+	mockgen -destination=model/mock/mock_utterance_service.go -package=mock cbupnvj/model UtteranceService
+
+
 
 mockgen: model/mock/mock_user_repository.go \
 	model/mock/mock_session_repository.go \
@@ -25,9 +80,35 @@ mockgen: model/mock/mock_user_repository.go \
 	model/mock/mock_user_service.go \
 	model/mock/mock_auth_service.go \
 	model/mock/mock_intent_service.go \
-	model/mock/mock_user_controller.go \
-	model/mock/mock_auth_controller.go \
-	model/mock/mock_intent_controller.go \
+	model/mock/mock_action_http_repository.go \
+	model/mock/mock_configuration_repository.go \
+	model/mock/mock_entity_repository.go \
+	model/mock/mock_example_repository.go \
+	model/mock/mock_faculty_repository.go \
+	model/mock/mock_fallback_chat_log_repository.go \
+	model/mock/mock_log_intent_repository.go \
+	model/mock/mock_major_repository.go \
+	model/mock/mock_req_body_repository.go \
+	model/mock/mock_rule_repository.go \
+	model/mock/mock_step_repository.go \
+	model/mock/mock_story_repository.go \
+	model/mock/mock_training_history_repository.go \
+	model/mock/mock_utterance_repository.go \
+	model/mock/mock_action_http_service.go \
+	model/mock/mock_configuration_service.go \
+	model/mock/mock_conversation_service.go \
+	model/mock/mock_entity_service.go \
+	model/mock/mock_example_service.go \
+	model/mock/mock_faculty_service.go \
+	model/mock/mock_fallback_chat_log_service.go \
+	model/mock/mock_log_intent_service.go \
+	model/mock/mock_major_service.go \
+	model/mock/mock_req_body_service.go \
+	model/mock/mock_rule_service.go \
+	model/mock/mock_step_service.go \
+	model/mock/mock_story_service.go \
+	model/mock/mock_training_history_service.go \
+	model/mock/mock_utterance_service.go \
 
 clean:
 	rm -v model/mock/mock_*.go
--- repository/user_repository_test.go
+++ repository/user_repository_test.go
@@ -1,11 +1,16 @@
 package repository
 
 import (
+	"cbupnvj/model"
 	"context"
+	"errors"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"gorm.io/gorm"
 )
 
 func TestUserRepository_FindByID(t *testing.T) {
@@ -20,11 +25,281 @@ func TestUserRepository_FindByID(t *testing.T) {
 	ctx := context.TODO()
 
 	t.Run("ok - retrieve from db", func(t *testing.T) {
-		mock.ExpectQuery("^SELECT .+ FROM \"users\"").
-			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(123))
+		mock.ExpectQuery("^SELECT \\* FROM `users` WHERE id = \\? AND `users`.`deleted_at` IS NULL LIMIT 1").
+			WithArgs("123").
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow("123"))
 
-		res, err := repo.FindByID(ctx, "")
+		res, err := repo.FindByID(ctx, "123")
 		require.NoError(t, err)
 		require.NotNil(t, res)
+	})
+
+	t.Run("not found", func(t *testing.T) {
+		mock.ExpectQuery("^SELECT \\* FROM `users` WHERE id = \\? AND `users`.`deleted_at` IS NULL LIMIT 1").
+			WithArgs("123").
+			WillReturnError(gorm.ErrRecordNotFound)
+
+		res, err := repo.FindByID(ctx, "123")
+		require.NoError(t, err)
+		require.Nil(t, res)
+	})
+}
+
+func TestUserRepository_Create(t *testing.T) {
+	kit, closer := initializeRepoTestKit(t)
+	defer closer()
+	mock := kit.dbmock
+
+	repo := userRepository{
+		db: kit.db,
+	}
+
+	ctx := context.TODO()
+
+	user := &model.User{
+		Id:        "123",
+		Email:     "test@gmail.com",
+		Password:  "hash",
+		Name:      "test",
+		Type:      model.UserAdmin,
+		MajorId:   "1",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	t.Run("ok", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec("^INSERT INTO `users`").
+			WithArgs(user.Id, user.Email, user.Password, user.Name, user.Type, user.MajorId, user.CreatedAt, user.UpdatedAt, user.DeletedAt).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectCommit()
+		err := repo.Create(ctx, user)
+		require.NoError(t, err)
+	})
+
+	t.Run("failed to insert user", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec("^INSERT INTO `users`").
+			WithArgs(user.Id, user.Email, user.Password, user.Name, user.Type, user.MajorId, user.CreatedAt, user.UpdatedAt, user.DeletedAt).
+			WillReturnError(errors.New("db error"))
+		err := repo.Create(ctx, user)
+		require.Error(t, err)
+	})
+}
+
+func TestUserRepository_Update(t *testing.T) {
+	kit, closer := initializeRepoTestKit(t)
+	defer closer()
+	mock := kit.dbmock
+
+	repo := userRepository{
+		db: kit.db,
+	}
+
+	ctx := context.TODO()
+
+	user := &model.User{
+		Id:        "123",
+		Email:     "test@gmail.com",
+		Password:  "hash",
+		Name:      "test",
+		Type:      model.UserAdmin,
+		MajorId:   "1",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	t.Run("ok", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec("^UPDATE `users`").
+			WithArgs(user.Password, user.Name, user.MajorId, sqlmock.AnyArg(), user.Id).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectCommit()
+		err := repo.Update(ctx, user.Id, user)
+		require.NoError(t, err)
+	})
+
+	t.Run("failed to update user", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec("^UPDATE `users`").
+			WithArgs(user.Password, user.Name, user.MajorId, sqlmock.AnyArg(), user.Id).
+			WillReturnError(errors.New("db error"))
+		err := repo.Update(ctx, user.Id, user)
+		require.Error(t, err)
+	})
+}
+
+func TestUserRepository_FindAll(t *testing.T) {
+	kit, closer := initializeRepoTestKit(t)
+	defer closer()
+	mock := kit.dbmock
+
+	repo := userRepository{
+		db: kit.db,
+	}
+
+	ctx := context.TODO()
+
+	t.Run("ok - found", func(t *testing.T) {
+		ids := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		resRows := sqlmock.NewRows([]string{"id"})
+		for _, v := range ids {
+			resRows.AddRow(v)
+		}
+		mock.ExpectQuery("^SELECT \\* FROM `users` WHERE type = \\? AND `users`.`deleted_at` IS NULL ORDER BY created_at DESC").
+			WithArgs(model.UserAdmin).
+			WillReturnRows(resRows)
+
+		res, err := repo.FindAll(ctx)
+		require.NoError(t, err)
+		require.NotNil(t, res)
+	})
+
+	t.Run("ok - not found", func(t *testing.T) {
+		resRows := sqlmock.NewRows([]string{"id"})
+
+		mock.ExpectQuery("^SELECT \\* FROM `users` WHERE type = \\? AND `users`.`deleted_at` IS NULL ORDER BY created_at DESC").
+			WithArgs(model.UserAdmin).
+			WillReturnRows(resRows)
+
+		res, err := repo.FindAll(ctx)
+		require.NoError(t, err)
+		require.NotNil(t, res)
+		assert.Equal(t, 0, len(res))
+	})
+
+	t.Run("error", func(t *testing.T) {
+		mock.ExpectQuery("^SELECT \\* FROM `users` WHERE type = \\? AND `users`.`deleted_at` IS NULL ORDER BY created_at DESC").
+			WithArgs(model.UserAdmin).
+			WillReturnError(gorm.ErrInvalidDB)
+
+		res, err := repo.FindAll(ctx)
+		require.Error(t, err)
+		require.Nil(t, res)
+	})
+}
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	kit, closer := initializeRepoTestKit(t)
+	defer closer()
+	mock := kit.dbmock
+
+	repo := userRepository{
+		db: kit.db,
+	}
+
+	ctx := context.TODO()
+
+	user := &model.User{
+		Id:    "123",
+		Email: "test@gmail.com",
+	}
+
+	t.Run("ok - retrieve from db", func(t *testing.T) {
+		mock.ExpectQuery("^SELECT `id` FROM `users` WHERE email = \\? AND `users`.`deleted_at` IS NULL LIMIT 1").
+			WithArgs(user.Email).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(user.Id))
+		mock.ExpectQuery("^SELECT \\* FROM `users` WHERE id = \\? AND `users`.`deleted_at` IS NULL LIMIT 1").
+			WithArgs(user.Id).
+			WillReturnRows(sqlmock.NewRows([]string{"id", "email"}).AddRow(user.Id, user.Email))
+		res, err := repo.FindByEmail(ctx, user.Email)
+		require.NoError(t, err)
+		require.NotNil(t, res)
+		assert.Equal(t, user, res)
+	})
+
+	t.Run("ok - not found", func(t *testing.T) {
+		mock.ExpectQuery("^SELECT `id` FROM `users` WHERE email = \\? AND `users`.`deleted_at` IS NULL LIMIT 1").
+			WithArgs(user.Email).
+			WillReturnError(errors.New("error db"))
+
+		res, err := repo.FindByEmail(ctx, user.Email)
+		require.Error(t, err)
+		require.Nil(t, res)
+	})
+}
+
+func TestUserRepository_ResetPassword(t *testing.T) {
+	kit, closer := initializeRepoTestKit(t)
+	defer closer()
+	mock := kit.dbmock
+
+	repo := userRepository{
+		db: kit.db,
+	}
+
+	ctx := context.TODO()
+
+	user := &model.User{
+		Id:        "123",
+		Email:     "test@gmail.com",
+		Password:  "newhash",
+		Name:      "test",
+		Type:      model.UserAdmin,
+		MajorId:   "1",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	t.Run("ok", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec("^UPDATE `users`").
+			WithArgs(user.Password, sqlmock.AnyArg(), user.Id).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectCommit()
+		err := repo.ResetPassword(ctx, user)
+		require.NoError(t, err)
+	})
+
+	t.Run("failed to update user", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec("^UPDATE `users`").
+			WithArgs(user.Password, sqlmock.AnyArg(), user.Id).
+			WillReturnError(errors.New("db error"))
+		err := repo.ResetPassword(ctx, user)
+		require.Error(t, err)
+	})
+}
+
+func TestUserRepository_Delete(t *testing.T) {
+	kit, closer := initializeRepoTestKit(t)
+	defer closer()
+	mock := kit.dbmock
+
+	repo := userRepository{
+		db: kit.db,
+	}
+
+	ctx := context.TODO()
+
+	user := &model.User{
+		Id:        "123",
+		Email:     "test@gmail.com",
+		Password:  "newhash",
+		Name:      "test",
+		Type:      model.UserAdmin,
+		MajorId:   "1",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	t.Run("ok", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec("^UPDATE `users`").
+			WithArgs(sqlmock.AnyArg(), user.Id).
+			WillReturnResult(sqlmock.NewResult(0, 1))
+		mock.ExpectCommit()
+		err := repo.Delete(ctx, user.Id)
+		require.NoError(t, err)
+	})
+
+	t.Run("failed to delete user", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec("^UPDATE `users`").
+			WithArgs(sqlmock.AnyArg(), user.Id).
+			WillReturnError(errors.New("db error"))
+		err := repo.Delete(ctx, user.Id)
+		require.Error(t, err)
 	})
 }
